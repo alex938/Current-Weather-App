d9129501d06f6593b5c4b0ed5219d218df041bf9
Alex <37157369+alex938@users.noreply.github.com>
2020-05-12 07:39:46 -0400

    Add files via upload

---
+++ weatherApp.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import requests
+from tkinter import font
+
+HEIGHT = 500
+WIDTH = 700
+
+def formattedWeather(w):
+    try:
+        place = w['name']
+        weather = w['weather'][0]['description']
+        temp = w['main']['temp']
+
+        output = "City: {}\nConditions: {}\nTemperature: {}c".format(place, weather, temp)
+
+    except:
+        output = "Error: cannot find location!\n\nPlease try entering its fullname."
+
+    return output    
+
+def getWeather(city):
+    key = 'd7d5e76abe1f62176dbd13bed47240e6'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    param = {'appid': key, 'q': city, 'units': 'metric'}
+    response = requests.get(url, params=param)
+    currentWeather = response.json()
+
+    label['text'] = formattedWeather(currentWeather)
+    
+
+
+#api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+
+root = tk.Tk()
+root.title("Alex's Weather App")
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+bgI = tk.PhotoImage(file='bg.png')
+bgL = tk.Label(root, image=bgI)
+bgL.place(x=0, y=0)
+
+frame = tk.Frame(root, bd=5, bg="#b3e0ff")
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+button = tk.Button(frame, font=('Arial', 20), text="Check", command=lambda:getWeather(entry.get()))
+button.place(relx=0.75, rely=0, relwidth=0.25, relheight=1)
+
+entry = tk.Entry(frame, font=('Arial', 20))
+entry.place(relx=0, rely=0, relwidth=0.7, relheight=1)
+
+frame2 = tk.Frame(root, bd=5, bg="#b3e0ff")
+frame2.place(relx=0.5, rely=0.3, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(frame2, bg="white", font=('Arial', 20))
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
